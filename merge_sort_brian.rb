597e211cd4351ba65c67a9fda305395ade0bb34c
Brian Groat <Barnum7@users.noreply.github.com>
2015-08-25 18:26:44 -0400

    Brian's merge sort

---
+++ merge_sort_brian.rb
@@ -0,0 +1,61 @@
+# Sort the array from lowest to highest
+#Got code here http://markmiyashita.com/interviews/problems/merge_sort/
+def sort(arr)
+      if arr.length <= 1
+    arr
+  else
+    mid = (arr.length / 2).floor
+    left = sort(arr[0..mid - 1])
+    right = sort(arr[mid..arr.length])
+    merge(left, right)
+  end
+end
+
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
+ end
+
+
+
+puts sort([12,5,93,91,16,17,95,87])
+=begin
+puts sort([15,4,28,4,8,1993,1986])
+
+# Find the maximum 
+def maximum(arr)
+  sort(arr).last
+end
+
+def minimum(arr)
+  sort(arr).first
+end
+ 
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return 2 below
+result = minimum([2, 42, 22, 02])
+puts "min of 2, 42, 22, 02 is: #{result}"
+
+ 
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+result = minimum([])
+puts "min on empty set is: #{result.inspect}"
+ 
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+ 
+result = maximum([6])
+puts "max of just 6 is: #{result}"
+=end
